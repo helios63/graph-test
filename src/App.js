45f2c7e4c4eb133b86197adef72d1bc2581a7ee9
Maxime Mondet <maxime.mondet@gmail.com>
2022-01-27 08:11:48 +0100

    ADDED : Fetch json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './styles.css';
+import React, {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('data.json')
+
+        if (response.status === 200) {
+          const results = await response.json()
+
+        }
+      } catch(errors) {
+        console.log(errors)
+      }
+    }
+
+    fetchData()
+
+  }, [])
+
   return (
     <div className="App">
       <div className='container'>
         <h1>New Graph</h1>
-        
+
       </div>
     </div>
   );
