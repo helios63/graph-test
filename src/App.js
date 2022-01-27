121f244c097210ec55ccdbaac252e54ddbd9fa26
Maxime Mondet <maxime.mondet@gmail.com>
2022-01-27 08:42:04 +0100

    Chart added to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './styles.css';
 import React, {useEffect} from 'react';
+import RadialChart from 'react-vis/dist/radial-chart';
 
 const App = () => {
 
+  // Fetching Data.json data
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -26,10 +28,25 @@ const App = () => {
 
   }, [])
 
+  // Calculate the ponderated score
+  const Calculate = () => {
+
+  }
+
+  const myData = [{angle: 1}, {angle: 5}, {angle: 2}]
+
   return (
     <div className="App">
       <div className='container'>
         <h1>New Graph</h1>
+
+        {/* Show GRAPH */}
+        <div className='chart'>
+          <RadialChart
+            data={myData}
+            width={780}
+            height={400} />
+        </div>
 
       </div>
     </div>
